5d6ed0c8ad674322d6426ca1c0822d1ac5fb0cca
Goutham kc <59509353+Gouthamkc@users.noreply.github.com>
2020-07-09 22:09:44 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -16,8 +16,9 @@ if(!message.member.hasPermission(["BAN_MEMBERS"])) return message.channel.send("
     }else{
     BanMember.send(`Hello, you have been banned from ${message.guild.name} for: ${reason}`).then(() => {
         message.channel.send(`**${BanMember.tag}** has been banned`)
-        message.guild.member(BanMember.id).ban(reasom);
-}).catch(err => console.log(err)
+        message.guild.member(BanMember.id).ban(reason);
+}).catch( message.channel.send(`**${BanMember.tag}** has been banned`)
+        message.guild.member(BanMember.id).ban(reason);)
 )
     }
 }
