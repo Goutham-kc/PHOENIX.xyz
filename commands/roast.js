91ae8aee1adffc16c08e287b240ac2c5a98b17e4
Gouthamkc <59509353+Gouthamkc@users.noreply.github.com>
2020-07-24 21:30:54 +0530

    Update roast.js

--- commands/roast.js
+++ commands/roast.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const request = require('request');
 module.exports.run = async (client, message, args) => {
-    const member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if (!member) return message.channel.send(`Please mention a a user to roast`);
     request('https://insult.mattbas.org/api/en/insult.txt?who=AAABBDD', async function(error, response, body) {
         if (!error && response.statusCode == 200) {
