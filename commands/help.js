8b5a9e2f12f54ef57ea4e0f5a797286aeef3b7c8
Gouthamkc <59509353+Gouthamkc@users.noreply.github.com>
2020-07-27 09:25:32 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,6 @@ module.exports.run = (client, message, args) => {
             .map(cmd => `\`${cmd.config.name}\``)
             .join(", "));
     }
-embed.setFooter(`[WebDashBoard](https://phoenixxyz-dashboard.herokuapp.com/)`)
        message.channel.send(embed)
     }
     if(arg.length !==0){
