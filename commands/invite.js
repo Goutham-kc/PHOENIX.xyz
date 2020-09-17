ca3169d304d71637573bcec94f7df69e715ee806
Gouthamkc <59509353+Gouthamkc@users.noreply.github.com>
2020-09-17 18:48:44 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
     .setAuthor("Links", message.author.avatarURL)
     .setColor("#00FFFF")
     .setThumbnail(message.author.displayAvatarURL({ size: 2048 }))
-    .addField(`Bot link:`,`** [Link](https://discord.com/api/oauth2/authorize?client_id=687987885604864033&permissions=1551367415&redirect_uri=https%3A%2F%2Fweb.phoenix-xyz.cf%2Fcallback&scope=bot)**`)
+    .addField(`Bot link:`,`** [Link](https://discord.com/api/oauth2/authorize?client_id=687987885604864033&permissions=1544027383&scope=bot)**`)
     . addField(`Support server: `, `**[Join now!!](https://discord.gg/RPVQdyw)**`)
     .setFooter(config.embed.footer)
     .setTimestamp();
