2fe56a7eef712cd1621dbbf07690c8336a44c7bd
Goutham kc <59509353+Gouthamkc@users.noreply.github.com>
2020-11-27 10:10:02 +0530

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 module.exports.run = async (client, message, args) => {
  const embed = new Discord.MessageEmbed() 
     message.channel.send(embed.setColor("BLUE").setDescription('Generating..')).then(m => { 
-        fetch('https://www.reddit.com/r/memes.json?sort=top&t=week')
+        fetch('https://www.reddit.com/r/meme/hot/.json?limit=100')
     .then(res => res.json()).then(body => {
   
   const SafeHandler = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
