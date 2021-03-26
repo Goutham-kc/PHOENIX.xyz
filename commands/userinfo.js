08ee39b68e626dc2a30ebfa054a1714ad537ec37
Shadow <59509353+Phoenix-Development@users.noreply.github.com>
2021-03-26 19:59:55 +0530

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 module.exports.run = async (client, message, args) => {
+    message.channel.send('Fixing...') return;
     const status = { online: "Online  <a:online:712855967653429308>", idle: "Idle <a:idle:688011476996325386>", dnd: "Do Not Disturb <a:dnd:712855705748766740>", offline: "Offline <a:offline:712856440435376239>" };
     let member = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
     if(!message.guild.member(member)) member = message.author
