326d4e4b91e71f1c7fde47214cb434edf59c2792
Shadow <59509353+Phoenix-Development@users.noreply.github.com>
2021-03-26 19:58:22 +0530

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args) => {
     .addField("Language:",`\`JavaScript\``,true)
     .addField('Library:',`\`discord.js ${require('discord.js').version}\``,true)
     .addField("Servers", `\`${client.guilds.cache.size}\``, true)
-    .addField("Users",`\`${client.users.cache.size}\``, true)
+    .addField("Users",`\`Broken\``, true)
     .addField('Ram Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
     .setFooter(config.embed.footer)
     message.channel.send(botembed);
